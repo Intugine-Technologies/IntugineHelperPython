dfaa4ab59c40bb3e1fa6e08d8dd4ac48a425c4e2
Intugine <rishabhdeep@intugine.com>
2019-06-07 13:01:48 +0530

    Version 0.6

--- intuginehelper/intuginehelper.py
+++ intuginehelper/intuginehelper.py
@@ -4,16 +4,26 @@ import math
 
 
 def square(x):
+    """
+    :param x:
+    :return: x * x
+    """
     return x * x
 
 
-def get_distane(src, dest):
-    Radius = 6371000  # Radius of the earth in m
-    dLat = math.radians(dest[0] - src[0])
-    dLon = math.radians(dest[1] - src[1])
-    a = square(math.sin(dLat / 2)) + math.cos(math.radians(src[0])) * math.cos(math.radians(dest[0])) * square(math.sin(dLon / 2))
+def get_distance(src, dest):
+    """
+    :param src: [lat, lng]
+    :param dest: [lat, lng]
+    :return: Distance in meters between src and dest
+    """
+    radius = 6371000  # Radius of the earth in m
+    d_lat = math.radians(dest[0] - src[0])
+    d_lon = math.radians(dest[1] - src[1])
+    a = square(math.sin(d_lat / 2)) + math.cos(math.radians(src[0])) * math.cos(math.radians(dest[0])) * square(
+        math.sin(d_lon / 2))
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    return math.floor(Radius * c)  # Distance in m
+    return math.floor(radius * c)  # Distance in m
 
 
 def get_database():
@@ -35,15 +45,17 @@ def get_running_trips():
 
 
 def get_all_pings(trips_list):
+    """
+    :param trips_list:
+    :return: list of['tripId' and 'pings' = [] ]
+    """
     trips_ids = [x['_id'] for x in trips_list]
     database = get_database()
     collection = database['status']
     try:
         data = collection.aggregate([{
             '$match': {
-                'tripId': {
-                    '$in': trips_ids
-                }
+                'tripId': {'$in': trips_ids}
             }
         }, {
             '$group': {
@@ -52,13 +64,21 @@ def get_all_pings(trips_list):
         }])
         return list(x for x in data)
     except Exception as e:
-        print(trips_ids)
         print(str(e))
         return []
 
 
 def get_days(diff):
     return diff.total_seconds() / (24 * 60 * 60)
+
+
+def get_days(start, end):
+    """
+    :param start: start datetime object
+    :param end: end datetime object
+    :return: time difference in days as float
+    """
+    return get_days(end - start)
 
 
 def get_eta_days(trip):
@@ -92,4 +112,3 @@ def get_eta_hrs(trip):
         except Exception as e:
             print(e)
             return -1
-
